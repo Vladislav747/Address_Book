d5cc4696464b213d1c62a0236466184596ac9612
Melo <zippo585@gmail.com>
2018-06-10 12:09:08 +0500

    Finished problem with execute (method onResume)

---
+++ app/src/main/java/com/example/melo/adress_book/IntentActivity.java
@@ -0,0 +1,28 @@
+package com.example.melo.adress_book;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+/**
+ * Created by Melo on 08.06.2018.
+ */
+
+public class IntentActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnHui:
+                Intent intent = new Intent(this, IntentActivity.class);
+                startActivity(intent);
+                break;
+                default:
+                    break;
+
+        }
+    }
+}
